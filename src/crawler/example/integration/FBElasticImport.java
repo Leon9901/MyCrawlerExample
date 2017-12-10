062f7663ce7ba2674691d86b90d589a430178c47
Leon <a29266053@gmail.com>
2017-12-10 15:56:26 +0800

    2017.12.9

--- src/crawler/example/integration/FBElasticImport.java
+++ src/crawler/example/integration/FBElasticImport.java
@@ -12,23 +12,23 @@ public class FBElasticImport {
 
     static String elasticHost = "dyn.gibar.co" ;
     static String elasticPort = "9200" ;
-    static String elasticIndex = "abola2";
+    static String elasticIndex = "leon9901";
     static String elasticIndexType = "data";
-    static String pageName = "JudgeAd";
+    static String pageName = "PttGossiping";
     // 2017-09-02
-    static long start = 1504363907;
+    static long start = 1512871818;
     // 往前抓抓取日期數
     static int days = 10;
     // 每日抓取文章上限 (上限1000)
     static int maxPosts = 10;
-    static String access_token = "EAACEdEose0cBAAuJhUlaZCuuqYnzXXXKPGY7Es4KZCLBLpnrIdCWjutfSl1AFZAdR3pFnF6PdrfmaU2EwyllSxhKAd7ZCIGfxZCeg8VkdspelNr9vVWIZCeBRnU5KUlZAPh9zamyT836FmdCadDfzwFILwR6hdMse7irLpIto5cnUnqSV4MtEAA2Hf8slFdXZBS489aSanZCYLAZDZD";
+    static String access_token = "EAACEdEose0cBALZB47DFVOh4OiOGGmI42puwUealHZCjyWsudOCVaPM7pE70DWF3GkuRfBaz1FSOLZB4l6Kb6LYqpbZCZCZCE0bfHrqs1bzXYkcKlxaf7Dzf8nb9EugMsP8e8p3XqUWxOxgZBHDoenYZCHeNYgbEdE8I9GLxziUFxLfu791puBcZBR4ZCoO6UXBIQZD";
 
     public static void main(String[] args) {
 
         for (long datatime = start ; datatime > start-86400*days ;datatime-=86400) {
             String uri =
                     "https://graph.facebook.com/v2.6"
-                            + "/"+pageName +"/posts?fields=message,comments.limit(0).summary(true),likes.limit(0).summary(true),created_time&since="+(datatime-3600*8)+"&until="+datatime+"&limit="+maxPosts
+                            + "/"+pageName +"/posts?fields=message,comments.limit(0).summary(true),likes.limit(0).summary(true),created_time&since="+(datatime-3600*24)+"&until="+datatime+"&limit="+maxPosts
                             + "&access_token="+access_token;
 
 
