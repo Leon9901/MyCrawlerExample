676c6ed57d0ccc00605d31766192d42eb596984f
Leon <a29266053@gmail.com>
2017-12-10 09:49:23 +0800

    2017.12.9

--- src/crawler/example/exam/FacebookExam.java
+++ src/crawler/example/exam/FacebookExam.java
@@ -24,9 +24,9 @@ public class FacebookExam {
 		// 遠端資料路徑
 
 		String uri = 
-				"https://graph.facebook.com/v2.6"
-				+ "/search?q="
-				+ "&access_token=";
+				"https://graph.facebook.com/v2.11"
+				+ "/judgead/posts?fields=id,link,message,created_time,likes.limit(0).summary(total_count),reactions.limit(0).summary(total_count)"
+				+ "&access_token=EAACEdEose0cBALZB47DFVOh4OiOGGmI42puwUealHZCjyWsudOCVaPM7pE70DWF3GkuRfBaz1FSOLZB4l6Kb6LYqpbZCZCZCE0bfHrqs1bzXYkcKlxaf7Dzf8nb9EugMsP8e8p3XqUWxOxgZBHDoenYZCHeNYgbEdE8I9GLxziUFxLfu791puBcZBR4ZCoO6UXBIQZD";
 
 
 		Elements elems =
@@ -41,7 +41,7 @@ public class FacebookExam {
 			String id = data.select("id").text();
 
 			// FIXIT
-			String reactions = "";
+			String reactions = data.select("reactions summary total_count").text();
 
 
 			output += id + "," + reactions + "\n";
